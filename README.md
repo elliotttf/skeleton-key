bbc0244eff3cffa51d2c7ddce814fda0a5f6e6b1
Elliott Foster <elliottf@codebrews.com>
2016-09-13 15:35:11 -0500

    :star: initial commit for skeleton key hash generator

---
+++ README.md
@@ -0,0 +1,67 @@
+# Skeleton Key
+
+_A simple object hash generator_
+
+This module will allow you to generate object hashes from a collection of values.
+The values can be inserted in any order and as long as they are equal a matching
+cache key can be generated.
+
+A common use case is cache key generation, where user input may dictate the
+conditions for the cached data. For example, a HTTP request to:
+
+`/users?filter[name]=sally&filter[occupation]=engineer`
+
+will yeild the same result as
+
+`/users?filter[occupation]=engineer&filter[name]=sally`
+
+but simply caching the request URI would result in two separate entries.
+
+## Usage
+
+```javascript
+const SkeletonKey = require('skeleton-key');
+
+route.use((req, res, next) => {
+  // Generate a cache key composed of the path and the filter.
+  const key = new SkeletonKey();
+  key.setNotch('path', req.path);
+  key.setNotch('filter', req.query.filter);
+  req.cacheKey = key.cut();
+  next();
+});
+```
+
+Now variance in the order of the filter query won't affect the cache key.
+
+You can also sort an array of values on add if the values are more important
+than the order to ensure consistency between keys, e.g.
+
+```javascript
+key.setNotch('fields', ['name', 'occupation', 'birthday'], true);
+```
+
+would generate the same notch as
+
+```javascript
+key.setNotch('fields', ['occupation', 'name', 'birthday'], true);
+```
+
+## API
+
+* `resetKey()` - Removes all notches from the key.
+* `setNotch(id, notch[, sort, [comparator]])` - Adds a notch to the key or
+  replaces an existing notch.
+   * id - The notch ID to set or replace.
+   * notch - The notch to add to the key.
+   * sort - A flag on whether or not the notch value should be sorted
+     before saving it to the key. This can be used to guarantee
+     consistency between keys when the order is not important but
+     the values are.
+   * comparator - An optional comparison function to use when sorting arrays.
+* `getNotch` - Gets the value of a given notch in the key.
+   * id - The notch ID to get.
+* `removeNotch` - Removes a notch from the key.
+   * id - The notch ID to remove from the key.
+* `cut` - Cuts the key, generating a MD5 hash of the map.
+
---
+++ test/index.js
@@ -0,0 +1,74 @@
+'use strict';
+const SkeletonKey = require('../');
+
+module.exports = {
+  setUp(cb) {
+    this.key = new SkeletonKey();
+    cb();
+  },
+  resetKey(test) {
+    test.expect(1);
+    this.key.setNotch('test', 'test');
+    this.key.resetKey();
+    test.equal('undefined', typeof this.key.getNotch('test'), 'Key not reset.');
+    test.done();
+  },
+  setNotch: {
+    simple(test) {
+      test.expect(1);
+      this.key.setNotch('test', 'test');
+      test.equal('test', this.key.getNotch('test'), 'Notch not set.');
+      test.done();
+    },
+    sorted(test) {
+      test.expect(1);
+      this.key.setNotch('test', [3, 2, 1], true);
+      test.deepEqual([1, 2, 3], this.key.getNotch('test'), 'Notch not sorted.');
+      test.done();
+    },
+    withComparator(test) {
+      test.expect(1);
+      this.key.setNotch('test', [1, 2, 3], true, (a, b) => (a > b ? -1 : 1));
+      test.deepEqual([3, 2, 1], this.key.getNotch('test'), 'Notch not sorted.');
+      test.done();
+    }
+  },
+  getNotch(test) {
+    test.expect(1);
+    this.key.setNotch('test', 'test');
+    test.equal('test', this.key.getNotch('test'), 'Notch not returned.');
+    test.done();
+  },
+  removeNotch(test) {
+    test.expect(1);
+    this.key.setNotch('test', 'test');
+    this.key.removeNotch('test');
+    test.equal('undefined', typeof this.key.getNotch('test'), 'Notch not removed.');
+    test.done();
+  },
+  cut: {
+    simple(test) {
+      test.expect(1);
+      this.key.setNotch('test', 'test');
+      test.equal('1b8d136b7e8c2abd66fb08f6b115b62e', this.key.cut(), 'Unexpected hash.');
+      test.done();
+    },
+    withObjects(test) {
+      test.expect(2);
+      this.key.setNotch('test', {a: 1, b: 2});
+      test.equal('a423fe10cbb06863a07d44ac5d2168df', this.key.cut(), 'Unexpected hash.');
+      this.key.setNotch('test', {b: 2, a: 1});
+      test.equal('a423fe10cbb06863a07d44ac5d2168df', this.key.cut(), 'Unexpected hash.');
+      test.done();
+    },
+    withSortedArrays(test) {
+      test.expect(2);
+      this.key.setNotch('test', [1, 2, 3]);
+      test.equal('21c354dd04e7aac0afc8e3e3693bba80', this.key.cut(), 'Unexpected hash.');
+      this.key.setNotch('test', [3, 2, 1], true);
+      test.equal('21c354dd04e7aac0afc8e3e3693bba80', this.key.cut(), 'Unexpected hash.');
+      test.done();
+    }
+  }
+};
+
